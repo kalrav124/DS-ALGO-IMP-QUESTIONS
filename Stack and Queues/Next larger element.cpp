8f315f40956601cb1cfa694f34e1f07ea9f30ee9
kalrav124 <36242337+kalrav124@users.noreply.github.com>
2021-01-27 17:29:01 +0530

    Update Next larger element.cpp

--- Stack and Queues/Next larger element.cpp
+++ Stack and Queues/Next larger element.cpp
@@ -1,3 +1,7 @@
+/*Given an array arr[ ] of size N having distinct elements, 
+the task is to find the next greater element for each element of the array in order of their appearance in the array.
+If there does not exist next greater of current element, then next greater element for current element is -1.
+*/
 
 vector<long long> nextLargerElement(vector<long long> arr, int n){
     stack<int>s;
