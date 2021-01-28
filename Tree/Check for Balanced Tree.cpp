13ee01bfbd802f8ccdf8471d780618be7ebd4850
kalrav124 <36242337+kalrav124@users.noreply.github.com>
2021-01-28 16:41:31 +0530

    Update Check for Balanced Tree.cpp

--- Tree/Check for Balanced Tree.cpp
+++ Tree/Check for Balanced Tree.cpp
@@ -8,7 +8,7 @@ int calcHeight(Node*root){
 bool isBalanced(Node *root)
 {
     if(root==NULL) return true;
-    int lh=calcHeight(root->left);
+    int lh=calcHeight(root->left);//rh=right height,lh=left height.
     int rh=calcHeight(root->right);
     return (abs(lh-rh)<=1 && isBalanced(root->left) && isBalanced(root->right));
 }
