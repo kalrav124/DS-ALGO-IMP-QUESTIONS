efb95817b56f272ddfbcea50dbf1f11f32eee5d5
kalrav124 <36242337+kalrav124@users.noreply.github.com>
2021-01-28 16:40:38 +0530

    Create Check for Balanced Tree.cpp

---
+++ Tree/Check for Balanced Tree.cpp
@@ -0,0 +1,14 @@
+// This function should return tree if passed  tree 
+// is balanced, else false. 
+
+int calcHeight(Node*root){
+    if(root==NULL) return 0;
+    return max(calcHeight(root->left),calcHeight(root->right))+1;
+}
+bool isBalanced(Node *root)
+{
+    if(root==NULL) return true;
+    int lh=calcHeight(root->left);
+    int rh=calcHeight(root->right);
+    return (abs(lh-rh)<=1 && isBalanced(root->left) && isBalanced(root->right));
+}
