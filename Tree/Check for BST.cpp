f3492d592eda86d5d14d73822ea1fbe1df5fd64e
kalrav124 <36242337+kalrav124@users.noreply.github.com>
2021-01-28 15:56:15 +0530

    Update Check for BST.cpp

--- Tree/Check for BST.cpp
+++ Tree/Check for BST.cpp
@@ -2,8 +2,10 @@
 
 int isBSTUtil(Node*root,int minimum,int maximum){
     if(root==NULL) return true;
-    return (root->data>minimum && root->data<maximum && isBSTUtil(root->left,
-    minimum,root->data)&&isBSTUtil(root->right,root->data,maximum));
+    return (root->data>minimum && root->data<maximum 
+            && isBSTUtil(root->left,minimum,root->data)
+            && isBSTUtil(root->right,root->data,maximum)
+           );
 }
 // return true if the given tree is a BST, else return false
 bool isBST(Node* root) {
